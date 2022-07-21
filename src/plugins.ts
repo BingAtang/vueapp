43abc0c111ecd47b7648c00b21b620ba2d1661cc
JacobZha <jacobzha@sf-express.com>
2022-07-21 18:40:48 +0800

    clean

--- src/plugins.ts
+++
@@ -1,6 +0,0 @@
-import { App } from "vue";
-
-export default function definePlugins(app: App): App {
-  // Define your plugins here with app.use. It is required for storybook support.
-  return app;
-}
